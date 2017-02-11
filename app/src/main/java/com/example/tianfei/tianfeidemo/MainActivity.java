37b63f44382613935f00788feed727685fe59632
tianfeifei <574067963@qq.com>
2017-02-12 02:09:40 +0800

    第一提交

---
+++ app/src/main/java/com/example/tianfei/tianfeidemo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.tianfei.tianfeidemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lzy.okgo.OkGo;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
