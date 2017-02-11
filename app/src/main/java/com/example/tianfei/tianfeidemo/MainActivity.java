7171ed3053d4e376d9800376e7f1f08599cbb319
tianfeifei <574067963@qq.com>
2017-02-12 02:38:00 +0800

    方法

--- app/src/main/java/com/example/tianfei/tianfeidemo/MainActivity.java
+++ app/src/main/java/com/example/tianfei/tianfeidemo/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.tianfei.tianfeidemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.lzy.okgo.OkGo;
 
 public class MainActivity extends AppCompatActivity {
 
